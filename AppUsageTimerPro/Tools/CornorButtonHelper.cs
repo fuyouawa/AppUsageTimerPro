10a3f266d6bf971537407d659ef8cb8976c1c29a
Noithw <2907142541@qq.com>
2024-03-12 22:26:42 +0800

    新增圆角按钮Style

---
+++ AppUsageTimerPro/Tools/CornorButtonHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppUsageTimerPro.Tools
+{
+    public static class CornorButtonHelper
+    {
+        public static readonly DependencyProperty CornorRadiusProperty =
+            DependencyProperty.RegisterAttached(
+                "CornorRadius",
+                typeof(double),
+                typeof(CornorButtonHelper),
+                new FrameworkPropertyMetadata(10.0));
+
+        public static double GetCornorRadius(DependencyObject obj)
+        {
+            return (double)obj.GetValue(CornorRadiusProperty);
+        }
+
+        public static void SetCornorRadius(DependencyObject obj, double value)
+        {
+            obj.SetValue(CornorRadiusProperty, value);
+        }
+    }
+}
